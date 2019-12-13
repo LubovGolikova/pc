077be51542e56e3a16ae568f172fd16a2f7cca8d
eromasters <evromastersx@gmail.com>
2019-12-13 18:55:10 +0200

    admin correct

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Edit {{$verse->name}}</h1>
+@stop
+
+@section('content')
+    @include('admin.errors')
+    @include('admin.success')
+    {!! Form::model($verse, ['route' => ['verses.update', $verse->id], 'files'=>true, 'method' => 'put']) !!}
+        @include('admin.verses.form')
+
+    {!! Form::submit('Save', ['class'=>'btn btn-primary']) !!}
+    {!! Form::close() !!}
+@stop
+
+@section('js')
+
+<script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
+<script src="/vendor/laravel-filemanager/js/lfm.js"></script>
+<script>
+  var options = {
+    filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+    filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+    filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+    filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+  };
+  CKEDITOR.replace('content', options);
+   $('#lfm').filemanager('image');
+</script>
+@endsection
+
+
+
