33d6d92c643351bf566afa7c8f002429cf659ede
eromasters <evromastersx@gmail.com>
2019-12-16 11:28:19 +0200

    test commit

--- app/Category.php
+++ app/Category.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    public  $timestamps = false;
     public function parent(){
         return $this->belongsTo('App\Category', 'parent_id');
     }
 
     function getParentName(){
         return $this->parent_id ? $this->parent->name : '';
+    }
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = \Str::slug($this->attributes['name'], '-');
     }
 }
