418dee9bf2dfe5e4a97c5dbc8030c9b973c0687e
eromasters <evromastersx@gmail.com>
2019-12-19 14:00:00 +0200

    user added

--- resources/views/verses/latest.blade.php
+++ resources/views/verses/latest.blade.php
@@ -4,7 +4,7 @@
     <h1>{{$title}}</h1>
     @foreach($verses as $verse)
         <article>
-            <a href="#">{{$verse->name}}</a>
+            <a href="/verses/{{$verse->id}}">{{$verse->name}}</a>
             <a href="#">{{$verse->user->name}}</a>
             <a href="#">{{$verse->category->name}}</a>
             {{\Carbon\Carbon::parse($verse->created_at)->format('d.m.Y')}}
